25ae60358229f0c119b4c8646cd79d3873d53c6f
shoni <juanigarcia@itba.edu.ar>
2022-06-08 08:24:15 -0300

    Change logger name

--- include/logger.h
+++ include/logger.h
@@ -2,28 +2,19 @@
 #define __logger_h_
 #include <stdio.h>
 #include <stdlib.h>
-
-/* 
-*  Macros y funciones simples para log de errores.
-*  EL log se hace en forma simple
-*  Alternativa: usar syslog para un log mas completo. Ver sección 13.4 del libro de  Stevens
-*/
+#include <stdarg.h>
 
 typedef enum {DEBUG=0, INFO, ERROR, FATAL} LOG_LEVEL;
 
 extern LOG_LEVEL current_level;
 
 /**
-*  Minimo nivel de log a registrar. Cualquier llamada a log con un nivel mayor a newLevel sera ignorada
+*  Minimo nivel de logger a registrar. Cualquier llamada a logger con un nivel mayor a newLevel sera ignorada
 **/
 void setLogLevel(LOG_LEVEL newLevel);
 
 char * levelDescription(LOG_LEVEL level);
 
-// Debe ser una macro para poder obtener nombre y linea de archivo. 
-#define log(level, fmt, ...)   {if(level >= current_level) {\
-	fprintf (stderr, "%s: %s:%d, ", levelDescription(level), __FILE__, __LINE__); \
-	fprintf(stderr, fmt, ##__VA_ARGS__); \
-	fprintf(stderr,"\n"); }\
-	if ( level==FATAL) exit(1);}
+void logger(LOG_LEVEL level, const char *fmt, ...);
+
 #endif
