bb7af48a899c5f7c2a65d8f4e91d05116915e249
shoni <juanigarcia@itba.edu.ar>
2022-06-21 09:12:39 -0300

    Fix typo

--- src/logger.c
+++ src/logger.c
@@ -21,7 +21,7 @@ void logger(LOG_LEVEL level, const char *fmt, ...){
     if(level >= current_level){
         time_t t = time(NULL);
         struct tm *tm = localtime(&t);
-        fprintf (out, "%d-%02d/-%02dT%02d:%02d:%02dZ\t",
+        fprintf (out, "%d-%02d-%02dT%02d:%02d:%02dZ\t",
                  tm->tm_year + 1900, tm->tm_mon, tm->tm_mday, tm->tm_hour, tm->tm_min, tm->tm_sec);
         va_list arg; 
         va_start(arg, fmt); 
